f6dc869ae86964807900b0ba49e3593ab8ada78f
yusei53 <yusei530922@gmail.com>
2024-07-30 18:59:59 +0900

    feat: authAPI.authRegister

---
+++ app/_api/auth/auth-api.ts
@@ -0,0 +1,24 @@
+import { AuthRegisterResponse } from "@/app/type/auth";
+import axios, { AxiosResponse } from "axios";
+
+const authAPI = {
+  async authRegister(
+    username: string,
+    email: string,
+    password: string,
+    githubId: string
+  ): Promise<AxiosResponse<AuthRegisterResponse>> {
+    const response = await axios.post<AuthRegisterResponse>(
+      "http://localhost:8080/api/auth/register",
+      {
+        username,
+        email,
+        password,
+        github_id: githubId,
+      }
+    );
+    return response;
+  },
+};
+
+export default authAPI;
